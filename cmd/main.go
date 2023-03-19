d60f2763fbc891ff9f06c7c083d5def447ea3097
Jonathan <silalahiboo@gmail.com>
2023-03-19 15:17:39 +0700

    refactor: move startapp function to internal

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/tokenomy-assessment/internal"
 	"github.com/tokenomy-assessment/internal/controller"
@@ -18,35 +15,7 @@ func main() {
 	m := http.NewServeMux()
 	controller.NewItemHandler(m, itemSvc)
 
-	if err := startApp(m, ":8089"); err != nil {
+	if err := internal.StartApp(m, ":8089"); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func startApp(m *http.ServeMux, addr string) error {
-	srv := &http.Server{
-		Addr:    addr,
-		Handler: m,
-	}
-
-	go func() {
-		log.Println("Server Start ", srv.Addr)
-		if err := internal.Start(srv); err != nil {
-			log.Fatalf("start: %s\n", err)
-		}
-	}()
-
-	quit := make(chan os.Signal)
-
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("Shutdown Server ...")
-
-	if err := internal.Shutdown(srv); err != nil {
-		return err
-	}
-
-	log.Println("Server exiting")
-
-	return nil
 }
--- internal/start.go
+++ internal/start.go
@@ -1,11 +1,37 @@
 package internal
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-func Start(
-	srv *http.Server,
-) (err error) {
-	return srv.ListenAndServe()
+func StartApp(m *http.ServeMux, addr string) error {
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: m,
+	}
+
+	go func() {
+		log.Println("Server Start ", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("start: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	if err := Shutdown(srv); err != nil {
+		return err
+	}
+
+	log.Println("Server exiting")
+
+	return nil
 }
