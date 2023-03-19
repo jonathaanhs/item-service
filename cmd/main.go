d4b291bf5fadf519ac8089f7be9b30fb367108c8
Jonathan <silalahiboo@gmail.com>
2023-03-19 15:03:50 +0700

    feat: add run http item

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/tokenomy-assessment/internal/controller"
+	"github.com/tokenomy-assessment/internal/service"
+)
+
+func main() {
+	itemSvc := service.NewItemSvc(service.ItemSvcImpl{})
+
+	m := http.NewServeMux()
+	controller.NewItemHandler(m, itemSvc)
+
+	srv := &http.Server{
+		Addr:    ":8089",
+		Handler: m,
+	}
+
+	log.Println("Server Start ", ":8089")
+
+	srv.ListenAndServe()
+}
